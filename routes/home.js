17cf8f7edba8c7be6493ce7d933bb4ed965f477c
vivekkumar1994 <96063498+vivekkumar1994@users.noreply.github.com>
2022-11-30 06:59:46 +0530

    api create

---
+++ routes/home.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const apiData = require("./data.json")
+
+router.get("/", async (req, res, next) => {
+  return res.status(200).json({
+    title: "Express Testing",
+    message: "The app is working properly!",
+  });
+});
+
+module.exports = router;
